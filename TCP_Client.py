d8de2d24a39b3f2d0cbef721b6524e30142d22de
escapeadmire <147107022+escapeadmire@users.noreply.github.com>
2025-10-22 14:18:46 -0600

    Uploading Initial Files

---
+++ TCP_Client.py
@@ -0,0 +1,12 @@
+import socket
+from socket import *    
+serverIP = '192.168.114.21'    
+serverport = 12000
+sentence = input('Input lowercase sentence: ')
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverIP, serverport))
+
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print('From Server:', modifiedSentence.decode())
+clientSocket.close()
